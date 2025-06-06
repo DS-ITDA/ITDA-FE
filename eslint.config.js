b11c118aecfb79f845000c4abb5d8efb3e8e078d
jinhyo <kjhyo0220@gmail.com>
2025-06-06 11:31:54 +0900

    🚧 Chore: eslint 설정 수정 (#1)
    eslint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
   { ignores: ['dist'] },
@@ -20,14 +20,12 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'prettier/prettier': ['warn'],
     },
   },
   {
@@ -36,4 +34,5 @@ export default [
       quotes: ['error', 'single'],
     },
   },
+  prettierConfig,
 ];
