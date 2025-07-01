15e66a3e2cdcf4ba4445c2510150f8e4ad8e9c65
jinhyo <kjhyo0220@gmail.com>
2025-07-01 15:15:40 +0900

    ✨ Feat: 메인페이지 책장 구현 (#6)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,27 @@
 import * as H from '@home/HomeStyle';
+import { useNavigate } from 'react-router-dom';
+import Books from '@components/Books/Books';
+import Arrow20 from '@assets/home/arrow-up_right-20.svg';
 
 const Home = () => {
-  return <H.Home>홈화면 - @@@님의 책장</H.Home>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <H.UserContainer>
+        <H.User>
+          <H.Title>
+            <p>
+              <span>조희원</span>님의 책장
+            </p>
+            <H.ArrowDiv onClick={() => navigate('/view')}>
+              <img src={Arrow20} alt="이미지 생성하러 가기" />
+            </H.ArrowDiv>
+          </H.Title>
+          <Books />
+        </H.User>
+      </H.UserContainer>
+    </>
+  );
 };
 
 export default Home;
