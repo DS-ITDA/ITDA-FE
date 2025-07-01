757b3a6036d6865dd1ee305a6aac0b188d86beb9
jinhyo <kjhyo0220@gmail.com>
2025-07-01 15:15:40 +0900

    ✨ Feat: 메인페이지 책장 구현 (#6)

--- src/pages/home/HomeStyle.js
+++ src/pages/home/HomeStyle.js
@@ -1,6 +1,62 @@
 import styled from 'styled-components';
 import palette from '@styles/theme';
 
-export const Home = styled.div`
-  font-family: HanSerif;
+export const Home = styled.div``;
+
+export const UserContainer = styled.div`
+  width: 100%;
+  padding: 0 10px;
+`;
+
+export const User = styled.div`
+  width: 100%;
+  background-color: ${palette.grayscale.white};
+  border-radius: 17px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 15px 15px 20px;
+`;
+
+export const Title = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 24px;
+
+  p {
+    margin: 0;
+  }
+
+  p > span {
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 130%;
+    letter-spacing: -0.4px;
+
+    margin-right: 2px;
+  }
+`;
+
+export const ArrowDiv = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: ${palette.main.ivory};
+  padding: 5px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
 `;
