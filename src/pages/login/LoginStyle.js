8ace2cd63cb51351476e870a08276631dca0ef30
mzxxzysy <jsyjj21@gmail.com>
2025-07-02 18:51:01 +0900

    ✨ Feat: 로그인 페이지 퍼블리싱 (#7)

--- src/pages/login/LoginStyle.js
+++ src/pages/login/LoginStyle.js
@@ -1,2 +1,46 @@
 import styled from 'styled-components';
-import palette from '@styles/theme';
+
+export const LoginPage = styled.div`
+  height: calc(100dvh - 130px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 90px 10px 40px;
+`;
+
+export const Logo = styled.img`
+  width: 143.912px;
+  height: 219.39px;
+`;
+
+export const Login = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const SocialLogin = styled.div`
+  width: 100%;
+  border-radius: 16px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  position: relative;
+`;
+
+export const SocialLogo = styled.img`
+  width: 24px;
+  height: 24px;
+
+  position: absolute;
+  top: 11px;
+  left: 20px;
+`;
+
+export const SocialText = styled.div`
+  padding: 13px 0;
+`;
