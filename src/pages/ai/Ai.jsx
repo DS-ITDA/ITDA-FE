ec4718c2502aa62fd21388e23cfb666d32047354
jinhyo <kjhyo0220@gmail.com>
2025-07-11 17:30:54 +0900

    ✨ Feat: 사진 업로드 및 스켈레톤 ui 구현 (#12)

--- src/pages/ai/Ai.jsx
+++ src/pages/ai/Ai.jsx
@@ -1,7 +1,42 @@
 import * as A from '@ai/AiStyle';
+import * as H from '@home/HomeStyle';
+import { useLocation } from 'react-router-dom';
 
 const Ai = () => {
-  return <div>AI분석</div>;
+  const location = useLocation();
+  const selectedImg = location.state?.selectedImg.thumbnail;
+
+  if (!selectedImg) return null;
+
+  return (
+    <A.Ai>
+      <H.UploadDiv>
+        <H.SelectedImg>
+          <img src={selectedImg} alt="사진 선택" />
+          <A.Scan />
+        </H.SelectedImg>
+      </H.UploadDiv>
+
+      <A.TextSkeleton>
+        <A.Skeleton $width={206} $height={20} />
+        <A.Skeleton $width={164} $height={40} />
+      </A.TextSkeleton>
+
+      <A.Wrapper>
+        <A.TopSkeleton>
+          <A.Skeleton $width={24} $height={24} />
+          <A.Skeleton $height={22} $flex={1} />
+        </A.TopSkeleton>
+
+        <A.GridSkeleton>
+          <A.Skeleton />
+          <A.Skeleton />
+          <A.Skeleton />
+          <A.Skeleton />
+        </A.GridSkeleton>
+      </A.Wrapper>
+    </A.Ai>
+  );
 };
 
 export default Ai;
