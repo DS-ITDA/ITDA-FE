f8fe82680d9b97f784e5686061e9bd5b60276257
jinhyo <kjhyo0220@gmail.com>
2025-07-12 12:47:45 +0900

    ♻️ Refactor: 스켈레톤 UI 컴포넌트로 분리 (#12)

---
+++ src/components/SkeletonUi/SkeletonUi.jsx
@@ -0,0 +1,41 @@
+import * as S from '@components/SkeletonUi/SkeletonUiStyle';
+import * as H from '@home/HomeStyle';
+import ToastMessage from '../common/ToastMessage/ToastMessage';
+
+const SkeletonUi = ({ selectedImg }) => {
+  return (
+    <S.Ai>
+      <H.UploadDiv>
+        <H.SelectedImg>
+          <img src={selectedImg} alt="사진 선택" />
+          <S.Scan />
+
+          <S.Message>
+            <ToastMessage text="스캔 중 ..." />
+          </S.Message>
+        </H.SelectedImg>
+      </H.UploadDiv>
+
+      <S.TextSkeleton>
+        <S.Skeleton $width={206} $height={20} />
+        <S.Skeleton $width={164} $height={40} />
+      </S.TextSkeleton>
+
+      <S.Wrapper>
+        <S.TopSkeleton>
+          <S.Skeleton $width={24} $height={24} />
+          <S.Skeleton $height={22} $flex={1} />
+        </S.TopSkeleton>
+
+        <S.GridSkeleton>
+          <S.Skeleton />
+          <S.Skeleton />
+          <S.Skeleton />
+          <S.Skeleton />
+        </S.GridSkeleton>
+      </S.Wrapper>
+    </S.Ai>
+  );
+};
+
+export default SkeletonUi;
--- src/pages/ai/Ai.jsx
+++ src/pages/ai/Ai.jsx
@@ -1,5 +1,6 @@
 import * as A from '@ai/AiStyle';
 import * as H from '@home/HomeStyle';
+import SkeletonUi from '../../components/SkeletonUi/SkeletonUi';
 import { useLocation } from 'react-router-dom';
 
 const Ai = () => {
@@ -9,33 +10,9 @@ const Ai = () => {
   if (!selectedImg) return null;
 
   return (
-    <A.Ai>
-      <H.UploadDiv>
-        <H.SelectedImg>
-          <img src={selectedImg} alt="사진 선택" />
-          <A.Scan />
-        </H.SelectedImg>
-      </H.UploadDiv>
-
-      <A.TextSkeleton>
-        <A.Skeleton $width={206} $height={20} />
-        <A.Skeleton $width={164} $height={40} />
-      </A.TextSkeleton>
-
-      <A.Wrapper>
-        <A.TopSkeleton>
-          <A.Skeleton $width={24} $height={24} />
-          <A.Skeleton $height={22} $flex={1} />
-        </A.TopSkeleton>
-
-        <A.GridSkeleton>
-          <A.Skeleton />
-          <A.Skeleton />
-          <A.Skeleton />
-          <A.Skeleton />
-        </A.GridSkeleton>
-      </A.Wrapper>
-    </A.Ai>
+    <>
+      <SkeletonUi selectedImg={selectedImg} />
+    </>
   );
 };
 
