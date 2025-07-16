2545b00c8377e43e96c71daa11a61e906f924284
mzxxzysy <jsyjj21@gmail.com>
2025-07-16 16:34:24 +0900

    ✨ Feat: 인터뷰 화면 구현 (#13)

--- src/pages/interview/Interview.jsx
+++ src/pages/interview/Interview.jsx
@@ -1,7 +1,101 @@
 import * as I from '@interview/InterviewStyle';
+import { useState } from 'react';
+import interviewBottom from '@assets/interview/interview-bottom.svg';
+import interviewStart from '@assets/interview/interview-start.svg';
+import interviewRecordOff from '@assets/interview/interview-record-off-90.svg';
+import interviewRecordOn from '@assets/interview/interview-record-on-90.svg';
+import interviewStopOff from '@assets/interview/interview-stop-off.svg-90.svg';
+import interviewStopOn from '@assets/interview/interview-stop-on-90.svg';
+import ToastMessage from '@components/common/ToastMessage/ToastMessage';
 
 const Interview = () => {
-  return <div>인터뷰</div>;
+  const [btnContent, setBtnContent] = useState('start');
+
+  const BtnContent_CONFIG = {
+    start: interviewStart,
+    recordReady: interviewRecordOff,
+    recordOn: interviewRecordOn,
+    stopOff: interviewStopOff,
+    stopOn: interviewStopOn,
+  };
+
+  return (
+    <I.InterviewPage>
+      {/* 페이지 콘텐츠 */}
+      {/* 첫 화면 */}
+      {btnContent === 'start' && (
+        <I.FirstInfo>
+          <I.Text>입력하신 정보를 확인했어요.</I.Text>
+          <I.Text style={{ marginBottom: '10px' }}>이제 회원님만의 스토리를 만들어볼까요?</I.Text>
+          <I.Text>
+            <I.Span>녹음이 가능</I.Span>한 환경에서,
+          </I.Text>
+          <I.Text>
+            <I.Span>아래 버튼</I.Span>을 눌러 <I.Span>인터뷰를 시작</I.Span>해보세요.
+          </I.Text>
+        </I.FirstInfo>
+      )}
+
+      {/* 녹음 준비 화면 - 스켈레톤 화면 */}
+      {btnContent === 'recordReady' && (
+        <I.ReadyPage>
+          <ToastMessage text={'준비 중...'} />
+
+          <I.MainPage>
+            <I.Skeleton $width={70} $height={20} />
+            <I.Skeleton $width={100} $height={20} />
+            <I.Skeleton $width={100} $height={20} />
+            <I.Skeleton $width={30} $height={6} style={{ marginLeft: '35%', marginTop: '35px' }} />
+          </I.MainPage>
+        </I.ReadyPage>
+      )}
+
+      {/* 질문 화면 */}
+      {btnContent === 'recordOn' && (
+        <I.QuestPage>
+          <ToastMessage text={'질문 중...'} />
+          <I.MainPage>
+            <I.MainText>
+              이 사진이 찍힐 때 설렘과 기대가 느껴졌다고 했어요. 어떤 일이 있었던 날이었는지 떠오르시나요?
+            </I.MainText>
+            <I.Divider />
+          </I.MainPage>
+        </I.QuestPage>
+      )}
+
+      {/* 답변 중 화면 */}
+      {btnContent === 'stopOn' && (
+        <I.AnswerPage>
+          <ToastMessage text={'답변 중...'} />
+          <I.MainPage>
+            <I.SubText>
+              이 사진이 찍힐 때 설렘과 기대가 느껴졌다고 했어요. 어떤 일이 있었던 날이었는지 떠오르시나요?
+            </I.SubText>
+            <I.SubDivider />
+            <I.MainText>
+              저는 이런 부분에서 설레고, 기대가 됐어요. 쇼타로와 함께 한 모든 시간들이 모두 좋았어요.
+            </I.MainText>
+          </I.MainPage>
+        </I.AnswerPage>
+      )}
+
+      {/* 하단 버튼 */}
+      <I.BottomContainer>
+        <I.BottomBtn>
+          <I.Button
+            src={BtnContent_CONFIG[btnContent]}
+            onClick={() => {
+              if (btnContent === 'start') setBtnContent('recordReady');
+              else if (btnContent === 'recordReady') setBtnContent('recordOn');
+              else if (btnContent === 'recordOn') setBtnContent('stopOn');
+              else if (btnContent === 'stopOn') setBtnContent('stopOff');
+            }}
+          />
+          <I.BtnBottom src={interviewBottom} />
+        </I.BottomBtn>
+      </I.BottomContainer>
+    </I.InterviewPage>
+  );
 };
 
 export default Interview;
--- src/pages/interview/InterviewStyle.js
+++ src/pages/interview/InterviewStyle.js
@@ -1,2 +1,177 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import palette from '@styles/theme';
+
+export const InterviewPage = styled.div`
+  width: 100%;
+  height: calc(100% - 90px);
+  margin: 30px 0;
+`;
+
+//첫 화면
+export const FirstInfo = styled.div`
+  margin-left: 20px;
+`;
+
+export const Text = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  font-size: 1.1rem;
+  font-weight: 400;
+  line-height: 140%;
+  letter-spacing: -0.28px;
+`;
+
+export const Span = styled.div`
+  font-size: 1.1rem;
+  font-weight: 600;
+  line-height: 140%;
+  letter-spacing: -0.28px;
+`;
+
+//녹음 준비 스켈레톤 화면
+export const ReadyPage = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30%;
+`;
+
+export const MainPage = styled.div`
+  width: calc(100% - 88px);
+  margin: 0 44px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+export const skeletonAnimation = keyframes`
+  0%{
+    background-color: rgba(234, 234, 234, 1);
+  }
+  50%{
+    background-color: rgba(234, 234, 234, 0.3);
+  }
+  100%{
+    background-color: rgba(234, 234, 234, 1);
+  }`;
+
+export const Skeleton = styled.div`
+  width: ${({ $width }) => ($width ? `${$width}%` : '100%')};
+  height: ${({ $height }) => ($height ? `${$height}px` : '100%')};
+  background-color: #eaeaea;
+  border-radius: 6px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${({ $flex }) => $flex === 1 && `flex:1`};
+
+  animation: ${skeletonAnimation} 1.5s infinite ease-in-out;
+`;
+
+//질문-답변 화면
+export const QuestPage = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 30%;
+`;
+
+export const AnswerPage = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 20%;
+`;
+
+//질문이 자연스럽게 위로 올라가는 애니메이션
+const slideUp = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(40px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const SubText = styled.div`
+  font-family: 'HanSerif';
+  font-size: 1.1rem;
+  font-weight: 400;
+  line-height: 140%;
+  letter-spacing: -0.32px;
+  color: ${palette.grayscale.gray};
+  animation: ${slideUp} 0.8s ease-out;
+`;
+
+export const MainText = styled.div`
+  font-family: 'HanSerif';
+  font-size: 1.1rem;
+  font-weight: 400;
+  line-height: 140%;
+  letter-spacing: -0.32px;
+  color: ${palette.grayscale.black};
+`;
+
+export const SubDivider = styled.div`
+  width: 60px;
+  height: 1px;
+  background-color: ${palette.grayscale.grayLight};
+  margin: 35px calc(50% - 30px);
+`;
+
+export const Divider = styled.div`
+  width: 60px;
+  height: 1px;
+  background-color: ${palette.grayscale.black};
+  margin: 35px calc(50% - 30px);
+`;
+
+//하단 버튼 스타일
+export const BottomContainer = styled.div`
+  position: fixed;
+  bottom: -5px;
+
+  width: 393px;
+  background-color: ${palette.main.ivory};
+
+  @media (max-width: 767px) {
+    width: 100vw;
+  }
+`;
+
+export const BottomBtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Button = styled.img`
+  width: 90px;
+  height: 90px;
+
+  position: absolute;
+  bottom: 45px;
+
+  @media (max-width: 767px) {
+    width: 27vw;
+    height: 27vw;
+    bottom: 12vw;
+  }
+`;
+
+export const BtnBottom = styled.img`
+  width: 100%;
+`;
