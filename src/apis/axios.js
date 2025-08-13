d8fb6b5bfbcec68b6fbe2644192335110284fca7
jinhyo <kjhyo0220@gmail.com>
2025-08-14 01:08:45 +0900

    🐛 Fix: 401 에러시 로그인 페이지로 이동 (#54)

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -17,3 +17,13 @@ axiosInstance.interceptors.request.use(
   },
   (error) => Promise.reject(error),
 );
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
