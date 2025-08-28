64bb452ae5a085fb91271e4f63863ba5c6533b0b
jinhyo <kjhyo0220@gmail.com>
2025-08-29 00:31:53 +0900

    ✏️ Typing Error: pwa 이름 수정 (#54)

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,9 @@ export default defineConfig({
         'favicons/favicon.svg',
       ],
       manifest: {
-        name: '잇다:그리움을곁에잇다',
-        short_name: '잇다:그리움을곁에잇다',
-        description: '잇다:그리움을곁에잇다',
+        name: '잇다: 그리움을 곁에 잇다',
+        short_name: '잇다: 그리움을 곁에 잇다',
+        description: '잇다:그리움을 곁에 잇다',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
